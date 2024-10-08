56a297d642ce52cf1c32fdd0df5ee35ff7566a1f
Yaswanth Niranjan <167855054+Yaswa12@users.noreply.github.com>
2024-10-08 15:38:00 -0400

    Update RotateImage.java

--- RotateImage.java
+++ RotateImage.java
@@ -6,7 +6,7 @@ class RotateImage{
     //transposing of the matrix
     private void transposeMatrix(int matrix[][]){
         for(int i=0;i<matrix.length;i++){
-            for (j=0;j<=i;j++){
+            for (int j=0;j<=i;j++){
                 int temp = matrix[i][j];
                 matrix[i][j]= matrix[j][i];
                 matrix[j][i]=temp;
